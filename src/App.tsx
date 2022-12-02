c7b26e14eb6961d5118fc492e4794d441ddfede6
Seungwoo Park <qpfgud430@gmail.com>
2022-12-02 21:56:03 +0900

    Initialize project using React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
-import Router from './routes/Router';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { darkTheme,lightTheme } from "./theme";
-import {useRecoilValue } from "recoil";
-import { isDarkAtom } from './atoms';
-
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import Router from "./routes/Router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { darkTheme, lightTheme } from "./theme";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "./atoms";
 
 const GlobalStyle = createGlobalStyle`
 	@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -56,26 +55,26 @@ const GlobalStyle = createGlobalStyle`
 	}
 	body {
 	  font-family: 'Source Sans Pro', sans-serif;
-	  background-color: ${props => props.theme.bgColor};
-	  color: ${props => props.theme.textColor};
+	  background-color: ${(props) => props.theme.bgColor};
+	  color: ${(props) => props.theme.textColor};
 	}
 	a {
 	  text-decoration: none;
 		color:inherit;
 	}
-	`
+	`;
 
-	function App() {
-		const isDark = useRecoilValue(isDarkAtom);
-	  return (
-			<>
-				<ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-	  		  <GlobalStyle />
-	  		  <Router />
-					<ReactQueryDevtools initialIsOpen={true}/>
-				</ThemeProvider>
-	  	</>
-	  )
-	}
+function App() {
+  const isDark = useRecoilValue(isDarkAtom);
+  return (
+    <>
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <Router />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>
+    </>
+  );
+}
 
 export default App;
