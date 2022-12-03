ec3458b84468e8a7209ff201c86b58fd6814d2de
Seungwoo Park <qpfgud430@gmail.com>
2022-12-03 15:09:41 +0900

    feat: theme color 설정

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,53 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme:DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "black",
-  accentColor: "#8c7ae6",
-}
-export const lightTheme:DefaultTheme = {
-  bgColor: "#F5DEB3",
-  textColor: "black",
-  accentColor: "#8C8072",
-}
+export const lightTheme: DefaultTheme = {
+  color: {
+    text: {
+      lv1: '#000000',
+      lv2: '#000000',
+    },
+    bg: {
+      lv1: '#f8f8f8',
+      lv2: '#ffffff',
+      lv3: '#f9f9f9',
+    },
+    accent: {
+      high: '#e02343',
+      low: '#1764b6',
+    },
+    grey: {
+      lv1: '#eeeeee',
+      lv2: '#f9f9f9',
+    },
+    mode: {
+      lv1: '#1f2a3c',
+      lv2: '#aab1b9',
+    },
+  },
+};
+
+export const darkTheme: DefaultTheme = {
+  color: {
+    text: {
+      lv1: '#aab1b9',
+      lv2: '#ffffff',
+    },
+    bg: {
+      lv1: '#1b2336',
+      lv2: '#1f2a3c',
+      lv3: '#2a384f',
+    },
+    accent: {
+      high: '#e02343',
+      low: '#1764b6',
+    },
+    grey: {
+      lv1: '#949caf',
+      lv2: '#f9f9f9',
+    },
+    mode: {
+      lv1: '#ffffff',
+      lv2: '#000000',
+    },
+  },
+};
