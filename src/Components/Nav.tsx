8e40b8c96e476b70509d18f22619e9042f07b897
Seungwoo Park <qpfgud430@gmail.com>
2022-12-04 23:20:26 +0900

    feat: Navbar 수정

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -9,7 +9,7 @@ const Header = styled.header`
   font-size: 40px;
   text-align: center;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   gap: 2rem;
   max-width: 1200px;
@@ -18,30 +18,15 @@ const Header = styled.header`
     padding: 0 20px;
   }
 `;
-const Logo = styled.h1`
-  font-style: oblique;
-  font-weight: 500;
-  font-size: 4rem;
-  word-break: keep-all;
-  @media screen and (max-width: 480px) {
-    font-size: 3em;
-  }
-  @media screen and (max-width: 390px) {
-    font-size: 2.8em;
-    flex-shrink: 1;
-  }
-`;
 
 export const Nav = () => {
   const setDarkAtom = useSetRecoilState(isDarkAtom);
-  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
   const isDark = useRecoilValue(isDarkAtom);
+  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
 
   return (
     <Header>
-      <Logo>
-        <Link to={"/"}>{`Coin Traker`}</Link>
-      </Logo>
+      <Link to={"/"}>{`Coin Traker`}</Link>
       <ModeChangeBtn onClick={toggleDarkAtom}>
         {isDark ? "Light" : "Dark"}
       </ModeChangeBtn>
